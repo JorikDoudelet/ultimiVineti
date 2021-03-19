4c2ee2128df0c54a9c9441130fb6bc29f9ac854f
Doudelet Jorik <jorik.doudelet@student.ehb.be>
2021-03-19 13:31:33 +0100

    navigation

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCOH2jijlHwsZ6OPhEK53NIFVxri_vIvGY",
+  authDomain: "ultimi-vineti.firebaseapp.com",
+  projectId: "ultimi-vineti",
+  storageBucket: "ultimi-vineti.appspot.com",
+  messagingSenderId: "851522661257",
+  appId: "1:851522661257:web:64c8daba614db5fd53cf58",
+  measurementId: "G-D5GFCWW4XQ",
+};
